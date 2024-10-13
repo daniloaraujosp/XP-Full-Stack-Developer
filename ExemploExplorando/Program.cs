68aef626c56736f0ddfdaff35914752fc71bfa4e
daniloaraujosp <danilo.araujo.sp@gmail.com>
2024-10-13 11:43:10 -0300

    UP Manipulando Valores com C# 01

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,6 +1,47 @@
 ﻿using ExemploExplorando.Models;
 
+int numero1 = 10;
+int numero2 = 20;
 
+int resultado = numero1 + numero2;
+
+Console.WriteLine(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 Pessoa p1 = new Pessoa(nome: "Danilo", sobrenome: "Araujo");
 Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Neves Queiroz");
 
@@ -20,3 +61,4 @@ cursoDeIngles.ListarAlunos();
 //p1._nome = ""; 
 
 //p1.Apresentar();
+*/
--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63ddd8f0c59f98a28f407293a3afe976b94604c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e890395a5eaf2623fc7922942516b58de5f358")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

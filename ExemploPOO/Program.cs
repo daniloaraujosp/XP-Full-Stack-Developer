678af3a88cf74a224ad45f04d0d4989f9dddef79
daniloaraujosp <danilo.araujo.sp@gmail.com>
2024-11-01 14:29:03 -0300

    UP Introdução, Abstração e Encapsulamento com C#

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExemploPOO.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Danilo";
+p1.Idade = 40;
+
+p1.Apresentar();
+*/
